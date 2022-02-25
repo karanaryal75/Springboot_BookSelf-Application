ad17e5d3efe5d604c1cce64ed9f412e4af162ca6
Karan Aryal <Karan Aryal>
2022-02-24 19:54:08 -0700

    Connection with cassandra

--- src/main/java/io/selfproject/bookselfdataloader/BookselfDataLoaderApplication.java
+++ src/main/java/io/selfproject/bookselfdataloader/BookselfDataLoaderApplication.java
@@ -1,13 +1,35 @@
 package io.selfproject.bookselfdataloader;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.*;
+
+/**
+ * Main application class with main method that runs the Spring Boot app
+ */
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BookselfDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookselfDataLoaderApplication.class, args);
 	}
+
+    /**
+     *Spring Boot app use the Astra secure bundle to connect to the database
+     */
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+	
 
 }
